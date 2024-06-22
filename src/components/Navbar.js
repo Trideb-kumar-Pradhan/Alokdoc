079798ea584a292ac34fb5b052950eb147694968
Trideb Pradhan <Trideb.Pradhan@alokind.com>
2024-06-22 17:06:44 +0530

    2stcommit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,103 +1,4 @@
 
-// import React, { useState } from 'react';
-// import { AppBar, Toolbar, Typography, Button, Menu, MenuItem, Dialog, DialogContent } from '@mui/material';
-// import { Worker, Viewer } from '@react-pdf-viewer/core';
-// import '@react-pdf-viewer/core/lib/styles/index.css';
-
-// const navItems = [
-//   { name: 'MM', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['MASTER DATA', 'POY/FDY PACKING', 'PSF/RPSF PACKING', 'DTY PACKING', 'ERROR HANDLING'] },
-//   { name: 'PPPI', pdfs: ['A.pdf', 'B.pdf', 'E.pdf', 'C.pdf', 'D.pdf'], displayNames: ['MASTER DATA', 'POY/FDY PACKING', 'PSF/RPSF PACKING', 'DTY PACKING', 'ERROR HANDLING'] },
-//   { name: 'SD', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['Product 1', 'MASTER DATA', 'Product 3', 'Product 4', 'Product 5'] },
-//   { name: 'EWM', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['MASTER DATA', 'Product 2', 'Product 3', 'Product 4', 'Product 5'] },
-//   { name: 'QM', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['MASTER DATA', 'Product 2', 'Product 3', 'Product 4', 'Product 5'] },
-//   { name: 'PM', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['Product 1', 'Product 2', 'Product 3', 'Product 4', 'Product 5'] },
-//   { name: 'MDG', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['Product 1', 'Product 2', 'Product 3', 'Product 4', 'Product 5'] },
-// ];
-
-// const Navbar = () => {
-//   const [anchorEl, setAnchorEl] = useState(null);
-//   const [selectedItem, setSelectedItem] = useState(null);
-//   const [dialogOpen, setDialogOpen] = useState(false);
-//   const [selectedPDF, setSelectedPDF] = useState(null);
-
-//   const handleClick = (event, item) => {
-//     setSelectedItem(item);
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//   };
-
-//   const handleView = (pdf) => {
-//     setSelectedPDF(pdf);
-//     setDialogOpen(true);
-//   };
-
-//   const handleDialogClose = () => {
-//     setDialogOpen(false);
-//   };
-
-//   return (
-//     <>
-//       <AppBar position="static" style={{ backgroundColor: '#1E90FF', boxShadow: 'none' }}>
-//         <Toolbar>
-//           <Typography
-//             variant="h3"
-//             component="div"
-//             sx={{ flexGrow: 1, fontFamily: 'serif', textAlign: 'flex-start', fontSize: '1.5rem', color: 'black', display: 'flex', alignItems: 'center', height: '50px' }}
-//           >
-//             <img src="/lo.png" alt="Company Logo" style={{ height: '55px', marginRight: '0.5rem', borderRadius: "5px",boxShadow: '0px 3px 5px rgba(14, 13, 15, 4.9)', }} />
-//             <span style={{ fontSize:'25px',fontFamily: 'Times New Roman, Times, serif',}}>ALOK INDUSTRIES SILVASSA</span>
-//           </Typography>
-//           {navItems.map((item, index) => (
-//             <div key={index} style={{ position: 'relative' }}>
-// <Button
-//   aria-controls={`simple-menu-${index}`}
-//   aria-haspopup="true"
-//   onClick={(e) => handleClick(e, item)}
-//   color="inherit"
-//   style={{
-//     boxShadow: '0px 3px 5px rgba(4, 3, 6, 0.4)', // Example box shadow
-//     margin: '5px', // Example padding values (adjust as needed)
-//   }}
-// >
-//                 {item.name}
-//               </Button>
-//               <Menu
-//                 id={`simple-menu-${index}`}
-//                 anchorEl={anchorEl}
-//                 keepMounted
-//                 open={Boolean(anchorEl && selectedItem === item)}
-//                 onClose={handleClose}
-//               >
-//                 {item.pdfs.map((pdf, idx) => (
-//                   <MenuItem key={idx} onClick={() => handleView(pdf)}>
-//                     {item.displayNames[idx]}
-//                   </MenuItem>
-//                 ))}
-//               </Menu>
-//             </div>
-//           ))}
-//         </Toolbar>
-//       </AppBar>
-
-//       <Dialog open={dialogOpen} onClose={handleDialogClose} maxWidth="lg" fullWidth>
-//         <DialogContent>
-//           {selectedPDF && (
-//             <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-//               <div style={{ height: '750px' }}>
-//                 <Viewer fileUrl={`/pdf/${selectedPDF}`} />
-//               </div>
-//             </Worker>
-//           )}
-//         </DialogContent>
-//       </Dialog>
-//     </>
-//   );
-// };
-
-// export default Navbar;
 
 
 import React, { useState } from 'react';
@@ -113,6 +14,7 @@ const navItems = [
   { name: 'QM', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['MASTER DATA', 'Product 2', 'Product 3', 'Product 4', 'Product 5'] },
   { name: 'PM', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['Product 1', 'Product 2', 'Product 3', 'Product 4', 'Product 5'] },
   { name: 'MDG', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['Product 1', 'Product 2', 'Product 3', 'Product 4', 'Product 5'] },
+  { name: 'PPPI PACKING MODULE', pdfs: ['A.pdf', 'E.pdf', 'D.pdf', 'C.pdf', 'B.pdf'], displayNames: ['Product 1', 'Product 2', 'Product 3', 'Product 4', 'Product 5'] },
 ];
 
 const Navbar = () => {
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,32 +1,62 @@
+
 import React from 'react';
-import { Typography, Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './section.css';
 
 const Section = () => {
   return (
     <Box
-    sx={{
-        height: '100vh',
-        backgroundImage: `url('/images/bgpic.jpg')`,
-        backgroundSize: 'cover', // Ensures the image covers the entire box without distortion
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
+      sx={{
         display: 'flex',
-        justifyContent: 'flex-end', // Align content to the flex-end of the container
-        paddingRight: '5rem', 
-        alignItems: 'center', // Centers items vertically
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh', 
+        backgroundColor: 'grey',
+        backgroundSize: 'cover', 
+        backgroundRepeat: 'no-repeat',
         color: 'white',
         textAlign: 'center',
+        overflow: 'hidden', 
+        position: 'relative', /* Ensure relative positioning for absolute children */
       }}
     >
-    <Typography variant="h3" style={{ color: 'grey',  }}>
-      Welcome to Alok DOCS
-    </Typography>
+      <Typography variant="h5" className="carousel-heading">
+        Welcome to Alok DOCS
+      </Typography>
+      <div className="carousel-wrapper">
+        <Carousel
+          showThumbs={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={4000}
+          stopOnHover={false}
+          className="carousel"
+        >
+          <div className="slide">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/a.jpg`}
+              alt="Slide 1" 
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/k.webp`}
+              alt="Slide 2"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/sm.jpg`}
+              alt="Slide 3"
+            />
+          </div>
+        </Carousel>
+      </div>
     </Box>
   );
 };
 
 export default Section;
-
-
-
-
